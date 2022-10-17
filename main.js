2ebfc9e0a3e57c0146ad69faa2cc4529e1737e3f
crazykenny93 <crazykenny593@gmail.com>
2022-10-17 10:12:21 +0200

    wip: migrate to currentPowerFlow API

--- main.js
+++ main.js
@@ -2,15 +2,27 @@
 
 /*
  * Solaredge Monitoring
- * github.com/92lleo/ioBroker.solaredge
+ * github.com/crazykenny93/ioBroker.solaredge
  *
- * (c) 2019 Leonhard Kuenzler (MIT)
+ * (c) crazykenny93 (MIT)
  *
  * Created with @iobroker/create-adapter v1.18.0
  */
 
 const utils = require("@iobroker/adapter-core");
 const request = require('request');
+const {stat} = require("fs");
+
+const states = [
+    "lastUpdateTime",
+    "exportedEnergy",
+    "importedEnergy",
+    "pvProduction",
+    "batteryDischarge",
+    "batteryCharge",
+    "load"
+]
+
 
 /**
  * The adapter instance
@@ -18,7 +30,7 @@ const request = require('request');
  */
 let adapter;
 let timer;
-let createStates;
+let shouldCreateStates;
 let siteid;
 
 /**
@@ -47,139 +59,145 @@ function startAdapter(options) {
     }));
 }
 
-function checkStateCreationNeeded(stateName){
+function checkStateCreationNeeded(stateName) {
     adapter.instance
     adapter.getState('solaredge.' + adapter.instance + '.' + siteid + '.' + stateName, function (err, state) {
         if (!state) {
-            adapter.log.info("state "+stateName+" does not exist, will be created");
-            createStates = true;
+            adapter.log.info("state " + stateName + " does not exist, will be created");
+            shouldCreateStates = true;
         } else {
-            adapter.log.debug("state "+stateName+" exists");
-            createStates |= false;
+            adapter.log.debug("state " + stateName + " exists");
+            shouldCreateStates |= false;
         }
     });
 }
 
-function checkStatesCreationNeeded(){
-    checkStateCreationNeeded('lastUpdateTime');
-    checkStateCreationNeeded('currentPower');
-    checkStateCreationNeeded('lifeTimeData');
-    checkStateCreationNeeded('lastYearData');
-    checkStateCreationNeeded('lastMonthData');
-    checkStateCreationNeeded('lastDayData');
+function createStates() {
+    let callback = function (val) {
+    }
+
+    adapter.log.debug("creating states");
+
+    adapter.createState('', siteid, 'lastUpdateTime', {
+        name: "lastUpdateTime",
+        type: 'string',
+        read: true,
+        write: false,
+        role: 'value',
+        desc: 'Last update from inverter'
+    }, callback);
+
+    adapter.createState('', siteid, 'exportedEnergy', {
+        name: "exportedEnergy",
+        type: 'number',
+        read: true,
+        write: false,
+        role: 'value',
+        desc: 'current exported energy'
+    }, callback);
+
+    adapter.createState('', siteid, 'importedEnergy', {
+        name: "importedEnergy",
+        type: 'number',
+        read: true,
+        write: false,
+        role: 'value',
+        desc: 'current imported energy'
+    }, callback);
+
+    adapter.createState('', siteid, 'pvProduction', {
+        name: "pvProduction",
+        type: 'number',
+        read: true,
+        write: false,
+        role: 'value',
+        desc: 'current pv production'
+    }, callback);
+
+    adapter.createState('', siteid, 'batteryCharge', {
+        name: "batteryCharge",
+        type: 'number',
+        read: true,
+        write: false,
+        role: 'value',
+        desc: 'current battery charge'
+    }, callback);
+
+    adapter.createState('', siteid, 'load', {
+        name: "load",
+        type: 'number',
+        read: true,
+        write: false,
+        role: 'value',
+        desc: 'current load'
+    }, callback);
+
+    shouldCreateStates = false;
 }
 
 function main() {
 
     siteid = adapter.config.siteid;
-    var apikey = adapter.config.apikey;
+    const apikey = adapter.config.apikey;
 
     adapter.log.info("site id: " + siteid);
     adapter.log.info("api key: " + (apikey ? (apikey.substring(0, 4) + "...") : "not set"));
 
-    // adapter only works with siteid and api key set
     if ((!siteid) || (!apikey)) {
         adapter.log.error("siteid or api key not set")
     } else {
-        var resource = "overview";
+        const resource = "currentPowerFlow";
+        const url = `https://monitoringapi.solaredge.com/site/${siteid}/${resource}.json?api_key=${apikey}`;
 
-        // for some other resources the url itself might change
-        var url = "https://monitoringapi.solaredge.com/site/" + siteid + "/" + resource + ".json?api_key=" + apikey;
-
-        checkStatesCreationNeeded();
+        states.forEach(state => {
+            checkStateCreationNeeded(state);
+        });
 
         request({
                 url: url,
                 json: true
             },
             function (error, response, content) {
-                if (!error && response.statusCode == 200) {
+                if (!error && response.statusCode === 200) {
                     if (content) {
-
-                        var callback = function (val) {}
-
-                        var overview = content.overview;
-
-                        adapter.log.info("Current power for " + siteid + ": " + overview.currentPower.power + " W");
-
-                        if (createStates) {
-                            adapter.log.debug("creating states");
-                            // create all states, only needed on first start or after state deletion
-
-                            // last update time
-                            adapter.createState('', siteid, 'lastUpdateTime', {
-                                name: "lastUpdateTime",
-                                def: overview.lastUpdateTime,
-                                type: 'string',
-                                read: true,
-                                write: false,
-                                role: 'value',
-                                desc: 'Last update from inverter'
-                            }, callback);
-
-                            adapter.createState('', siteid, 'currentPower', {
-                                name: "currentPower",
-                                def: overview.currentPower.power,
-                                type: 'number',
-                                read: true,
-                                write: false,
-                                role: 'value',
-                                desc: 'current power in W'
-                            }, callback);
-
-                            adapter.createState('', siteid, 'lifeTimeData', {
-                                name: "lifeTimeData",
-                                def: overview.lifeTimeData.energy,
-                                type: 'number',
-                                read: true,
-                                write: false,
-                                role: 'value',
-                                desc: 'Lifetime energy in Wh'
-                            }, callback);
-
-                            adapter.createState('', siteid, 'lastYearData', {
-                                name: "lastYearData",
-                                def: overview.lastYearData.energy,
-                                type: 'number',
-                                read: true,
-                                write: false,
-                                role: 'value',
-                                desc: 'last year energy in Wh'
-                            }, callback);
-
-                            adapter.createState('', siteid, 'lastMonthData', {
-                                name: "lastMonthData",
-                                def: overview.lastMonthData.energy,
-                                type: 'number',
-                                read: true,
-                                write: false,
-                                role: 'value',
-                                desc: 'last month energy in Wh'
-                            }, callback);
-
-                            adapter.createState('', siteid, 'lastDayData', {
-                                name: "lastDayData",
-                                def: overview.lastDayData.energy,
-                                type: 'number',
-                                read: true,
-                                write: false,
-                                role: 'value',
-                                desc: 'last day energy in Wh'
-                            }, callback);
-
-                            createStates = false;
-
-                        } else {
-                            // just update
-                            adapter.log.debug("updating states");
-
-                            adapter.setStateChanged(siteid + '.lastUpdateTime', overview.lastUpdateTime, true);
-                            adapter.setStateChanged(siteid + '.currentPower', overview.currentPower.power, true);
-                            adapter.setStateChanged(siteid + '.lifeTimeData', overview.lifeTimeData.energy, true);
-                            adapter.setStateChanged(siteid + '.lastYearData', overview.lastYearData.energy, true);
-                            adapter.setStateChanged(siteid + '.lastMonthData', overview.lastMonthData.energy, true);
-                            adapter.setStateChanged(siteid + '.lastDayData', overview.lastDayData.energy, true);
+                        if (shouldCreateStates) {
+                            createStates();
                         }
+
+                        let currentPowerFlow = content.siteCurrentPowerFlow;
+                        let isKW = currentPowerFlow.unit === "kW";
+
+                        let pvPower = isKW ? currentPowerFlow.PV.currentPower * 1000 : currentPowerFlow.PV.currentPower;
+                        let batteryDischarge = 0;
+                        let batteryCharge = 0;
+                        let importedEnergy = 0;
+                        let exportedEnergy = 0;
+                        let load = 0;
+
+
+                        let storagePowerFlow = isKW ? currentPowerFlow.STORAGE.currentPower * 1000 : currentPowerFlow.STORAGE.currentPower;
+                        if (currentPowerFlow.connections.contains({from: "STORAGE", to: "Load"})) {
+                            batteryDischarge = storagePowerFlow;
+                        } else if (currentPowerFlow.connections.contains({from: "LOAD", to: "Storage"})) {
+                            batteryCharge = storagePowerFlow;
+                        }
+
+                        let gridPowerFlow = isKW ? currentPowerFlow.GRID.currentPower * 1000 : currentPowerFlow.GRID.currentPower;
+                        if (currentPowerFlow.connections.contains({from: "GRID", to: "Load"})) {
+                            importedEnergy = gridPowerFlow;
+                        } else if (currentPowerFlow.connections.contains({from: "Load", to: "GRID"})) {
+                            exportedEnergy = gridPowerFlow;
+                        }
+
+                        currentPowerFlow.connections.filter(c => c.to === "Load").forEach(c => {
+                            load += currentPowerFlow[c.from].currentPower;
+                        });
+
+
+                        adapter.log.debug("updating states");
+
+                        adapter.setStateChanged(siteid + '.lastUpdateTime', overview.lastUpdateTime, true);
+                        adapter.setStateChanged(siteid + '.excessEnergy', overview.currentPower.power, true);
                     } else {
                         adapter.log.warn('Response has no valid content. Check your data and try again. ' + response.statusCode);
                     }
@@ -189,11 +207,13 @@ function main() {
 
                 adapter.log.info("Done, stopping...");
                 adapter.stop();
-            });
+            }
+        )
+        ;
     }
 
-    // (force) stop adapter after 15s
-    timer = setTimeout(function() {
+// (force) stop adapter after 15s
+    timer = setTimeout(function () {
         adapter.log.warn("Timeout, stopping...");
         adapter.stop();
     }, 15000);
